72bb31707a2c0d80505d0e7e901b111975d5bf3b
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2018-12-24 18:46:53 +0100

    Configure firebase

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import flush from 'styled-jsx/server';
-import useragent from 'useragent';
-
 
 export default class MyDocument extends Document {
 
@@ -17,14 +15,13 @@ export default class MyDocument extends Document {
     const { req, res, renderPage, isServer = false } = props;
     const { html, head, errorHtml, chunks } = renderPage();
     const styles = flush();
-    const ua = useragent.parse(req.headers['user-agent']);
 
-    return { html, head, errorHtml, chunks, styles, isServer, useragent: ua };
+    return { html, head, errorHtml, chunks, styles, isServer };
   }
 
   render() {
     // SSR styles, avoid page to blink
-    const { styles, useragent } = this.props;
+    const { styles } = this.props;
     // console.log('styles', JSON.stringify(styles, null, 2))
 
     return (
@@ -33,14 +30,6 @@ export default class MyDocument extends Document {
           <style>{styles}</style>
           {/*<link rel="stylesheet" href={'https://storage.googleapis.com/unly/libs/animate.css'} />*/}
           <script src="https://storage.googleapis.com/studylink/libs/fontawesome-pro-5.3.1-web/js/all.min.js" />
-
-          { // XXX Add polyfill for IE, since it's lacking many basic features such as Array.find and won't work without them
-            useragent.family === 'IE' && (
-              <script
-                src="https://storage.googleapis.com/studylink-loan-advisor/babel-polyfill-6.23.0.min.js"
-              />
-            )
-          }
         </Head>
         <body>
           <Main />
