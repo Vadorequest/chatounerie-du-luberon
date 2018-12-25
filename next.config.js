96bf550092c034e5a3024b23a2a990eedeb98629
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2018-12-25 19:32:09 +0100

    Add robots.txt and copy it when building + set NODE_ENV when deploying

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 const withImages = require('next-images');
 const withCSS = require('@zeit/next-css');
+const fs = require('fs');
+const { join } = require('path');
+const { promisify } = require('util');
+const copyFile = promisify(fs.copyFile);
 
 module.exports = withCSS(withImages({
-  webpack(config, options) {
-    return config;
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    if (dev) {
+      return defaultPathMap;
+    }
+    await copyFile(join(dir, 'static/robots.txt'), join(outDir, 'robots.txt'));
+
+    return defaultPathMap;
   },
 }));
